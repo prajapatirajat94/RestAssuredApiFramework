096005947bc2aca753c24543aefdd5513a268eea
Your Name <prajapatirajat94@gmail.com>
2023-01-15 17:07:18 -0500

    Reportadded

--- src/test/java/com/qa/api/gorest/tests/GetUserTest.java
+++ src/test/java/com/qa/api/gorest/tests/GetUserTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 
 import com.qa.api.gorest.restclient.Restclient;
 
+import io.qameta.allure.Epic;
 import io.restassured.response.Response;
 
 public class GetUserTest {
